52deb2476c5aef21f8593aedeef3d1914540cf9a
BSzura <117302856+BSzura@users.noreply.github.com>
2025-01-23 12:49:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Konfiguracja klienta Discord
 const client = new Client({
