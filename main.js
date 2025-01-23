9f2bd4c3cb72896a0c89b3ee590ae24fff1b852a
BSzura <117302856+BSzura@users.noreply.github.com>
2025-01-23 12:05:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Self-bot działa!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer pingujący działa na porcie ${PORT}`);
+});
 client.on("ready", () => {
   console.log("Self-bot działa na Render!");
 });
